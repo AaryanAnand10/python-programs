e464cf4b6e9bf059f0e66c33ce955b08696d30e4
Aaryan Anand <144722275+AaryanAnand10@users.noreply.github.com>
2024-01-01 22:03:05 +0530

    Update file-handling-io.py

--- file-handling-io.py
+++ file-handling-io.py
@@ -1,6 +1,18 @@
-"""
-reading a text file
-"""
+#For a Text file
+--> Reading a File:-
 with open("text.txt",w) as file_object:
   rd=file_object.reader()
   
+#For a Binary file
+--> Reading a File:-
+with open('binary_file.bin', 'rb') as file:
+    (Read the entire content of the file)
+    binary_data = file.read()
+
+
+#For CSV file:-
+-->Readind a File:-
+import csv
+csv_file_path = 'your_file.csv'
+with open(csv_file_path, newline='') as csv_file:
+    csv_reader = csv.reader(csv_file)
