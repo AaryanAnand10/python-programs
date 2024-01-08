dd6f6714de1371ae5cc81140f329d786a28f978d
Aaryan Anand <144722275+AaryanAnand10@users.noreply.github.com>
2024-01-08 18:37:58 +0530

    Update file-handling-io.py

--- file-handling-io.py
+++ file-handling-io.py
@@ -2,12 +2,29 @@
 --> Reading a File:-
 with open("text.txt",w) as file_object:
   rd=file_object.reader()
-  
+
+
 #For a Binary file
 --> Reading a File:-
 with open('binary_file.bin', 'rb') as file:
     (Read the entire content of the file)
     binary_data = file.read()
+
+  Example for a binary code to read and display its contents:
+import struct
+
+# Write binary data to a file
+with open('binary_data.bin', 'wb') as binary_file:
+    binary_file.write(struct.pack('iid', 123, 45.67, 89.12))
+
+# Read binary data from the file
+with open('binary_data.bin', 'rb') as binary_file:
+    data = struct.unpack('iid', binary_file.read())
+
+# Display the data
+labels = ["Integer", "Float 1", "Float 2"]
+for label, value in zip(labels, data):
+    print(f"{label}: {value}")
 
 
 #For CSV file:-
